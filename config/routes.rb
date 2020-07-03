4ee7b48f98ef8efb0b8cc08719447ccdfd06e2ad
Sheena Bonilla <sheena.myb@gmail.com>
2020-07-03 03:56:07 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
-  get '/show' => 'secrets#show'
+  post '/show' => 'secrets#show'
   get '/secret' => 'secrets#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
