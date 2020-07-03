e05424fd2ee1bcd57237f2878de43db948205016
Sheena Bonilla <sheena.myb@gmail.com>
2020-07-03 04:21:45 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if session[:user].blank?
+    if session[:user].blank
       redirect_to "/login"
     else
       session[:name] = params[:name]
