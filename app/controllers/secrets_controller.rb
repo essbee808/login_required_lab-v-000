3bdf957ac20265d7e4a787e9911a45bd0941be79
Sheena Bonilla <sheena.myb@gmail.com>
2020-07-03 04:40:50 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -10,7 +10,7 @@ class SecretsController < ApplicationController
 
   def show
     if session[:name].present?
-      render secret_path
+      render 
     else
       redirect_to "/login"
     end
