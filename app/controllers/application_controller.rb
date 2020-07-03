1cbced183ad7417e25fc7ce7032f0b24a084582d
Sheena Bonilla <sheena.myb@gmail.com>
2020-07-03 04:01:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    raise params.inspect
     session[:name] == params[:name]
   end
 
