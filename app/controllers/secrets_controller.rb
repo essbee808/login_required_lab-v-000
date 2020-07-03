e6f156abd4d29a31ff0634e514c4b939c0645f11
Sheena Bonilla <sheena.myb@gmail.com>
2020-07-03 03:57:34 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -3,7 +3,7 @@ class SecretsController < ApplicationController
   skip_before_action :require_login, only: [:show]
 
   def show
-    if :current_user == params[:name]
+    if current_user == params[:name]
       redirect_to '/secret'
     else
       redirect_to '/login'
